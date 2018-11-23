dc0826889e1b07b5d2344fa01c50ea9cf7359f27
bbvch13531 <bbvch13531@gmail.com>
2018-11-23 15:46:47 +0900

    Add clickListener and LongClickListener
    Signed-off-by: bbvch13531 <bbvch13531@gmail.com>

--- TouchEventExample/app/src/main/java/com/example/kyungyoungheo/toucheventexample/TouchListener.java
+++ TouchEventExample/app/src/main/java/com/example/kyungyoungheo/toucheventexample/TouchListener.java
@@ -6,6 +6,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
+import android.view.View.OnLongClickListener;
 import android.widget.Button;
 
 public class TouchListener extends AppCompatActivity {
@@ -17,12 +18,34 @@ public class TouchListener extends AppCompatActivity {
 
         Button listenerButton = (Button) findViewById(R.id.listenerButton);
 
-        listenerButton.setOnTouchListener(new View.OnTouchListener(){
-            @Override
-            public boolean onTouch(View v, MotionEvent event) {
-                Log.i("TouchListener","Button View onTouch() >> " + event.getAction());
-                return false;
-            }
-        });
+//        listenerButton.setOnTouchListener(new MyTouchListener());
+        listenerButton.setOnClickListener(new MyClickListener());
+        listenerButton.setOnLongClickListener(new MyLongClickListener());
+    }
+
+    private class MyTouchListener implements View.OnTouchListener {
+
+        @Override
+        public boolean onTouch(View v, MotionEvent event) {
+            Log.i("TouchListener","Button View onTouch() >> " + event.getAction());
+            return false;
+        }
+    }
+
+    private class MyClickListener implements View.OnClickListener{
+
+        @Override
+        public void onClick(View v) {
+            Log.i("TouchListener","Button View onClick()");
+        }
+    }
+
+    private class MyLongClickListener implements View.OnLongClickListener {
+        @Override
+        public boolean onLongClick(View v) {
+            Log.i("TouchListener","Button View onLongLongClick()");
+            return false;
+        }
+
     }
 }
